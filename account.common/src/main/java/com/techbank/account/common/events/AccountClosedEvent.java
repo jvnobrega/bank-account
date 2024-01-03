4a05d78c0753222d231cb205778657694e59af79
Joao Victor Nobrega <j.victorng@gmail.com>
2024-01-03 09:06:07 +0000

    Command Dispatching and Mediator Pattern

---
+++ account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -0,0 +1,9 @@
+package com.techbank.account.common.events;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+import lombok.Data;
+
+@Data
+public class AccountClosedEvent extends BaseEvent {
+
+}
